11863f4782ea3f0d6ba8db1289aeaa3040035a67
Jens Elkner <jel+jelmilter-src@linofee.org>
2007-12-03 03:56:36 +0000

    initial import

---
+++ src/de/ovgu/cs/jelmilter/misc/MboxReader.java
@@ -0,0 +1,321 @@
+/**
+ * $Id$ 
+ * 
+ * Copyright (c) 2005-2007 Jens Elkner.
+ * All Rights Reserved.
+ *
+ * This software is the proprietary information of Jens Elkner.
+ * Use is subject to license terms.
+ */
+package de.ovgu.cs.jelmilter.misc;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.mail.BodyPart;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.SharedByteArrayInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.ovgu.cs.milter4j.util.Mail;
+import de.ovgu.cs.whois.Whois;
+
+/**
+ * Utility for testing/extraction
+ * 
+ * @author 	Jens Elkner
+ * @version	$Revision$
+ */
+public class MboxReader {
+	private static final Logger log = LoggerFactory
+		.getLogger(MboxReader.class);
+
+	/**
+	 * Empty init.
+	 */
+	public MboxReader() {
+		// later
+	}
+	
+	/**
+	 * Read an mbox file.
+	 * <p>
+	 * NOTE: Maps the whole file to memory and uses 
+	 * {@link SharedByteArrayInputStream} to work on it.
+	 * 
+	 * @param mbox		mbox file to read
+	 * @return	a possible empty list
+	 * @throws IOException
+	 */
+	public ArrayList<Mail> read(File mbox) throws IOException {
+		ArrayList<Mail> msg = new ArrayList<Mail>();
+		if (mbox == null) {
+			log.info("No mbox file to read - nothing to do");
+			return msg;
+		}
+		byte[] src = new byte[(int) mbox.length()];
+		int count = 0;
+		int i = 0;
+		FileInputStream ir = null;
+		try {
+			ir = new FileInputStream(mbox);
+			while ((i = ir.read(src, count, src.length - count)) != -1
+				&& count < src.length) 
+			{
+				count += i;
+			}
+		} finally {
+			try { ir.close(); } catch (Exception x) { /* ignore */ }
+		}
+		byte[] from = new byte[] { '\n', 'F', 'r', 'o', 'm', ' '};
+		int eol = 0;
+		while (src[eol] != '\n') {
+			eol++;
+		}
+		for (i=eol+1; i + from.length < src.length; i++) {
+			for (int k=from.length-1; k >= 0; k--) {
+				if (src[i+k] != from[k]) {
+					break;
+				}
+				if (k==0) {
+					// start of a new message
+					SharedByteArrayInputStream in = 
+						new SharedByteArrayInputStream(src, eol+1, i - eol -1);
+					try {
+						Mail m = new Mail(in);
+						msg.add(m);
+					} catch (MessagingException e) {
+						log.warn(e.getLocalizedMessage());
+						if (log.isDebugEnabled()) {
+							log.debug("method()", e);
+						}
+					}
+					eol = i+from.length;
+					while (src[eol] != '\n') {
+						eol++;
+					}
+					i = eol;
+				}
+			}
+		}
+		SharedByteArrayInputStream in = 
+			new SharedByteArrayInputStream(src, eol+1, src.length - eol -1);
+		try {
+			Mail m = new Mail(in);
+			msg.add(m);
+		} catch (MessagingException e) {
+			log.warn(e.getLocalizedMessage());
+			if (log.isDebugEnabled()) {
+				log.debug("method()", e);
+			}
+		}
+		log.info("found " + msg.size() + " messages");
+		return msg;
+	}
+	
+	private void dump(File file, byte[] content) {
+		FileOutputStream fos = null; 
+		try {
+			fos = new FileOutputStream(file);
+			fos.write(content);
+		} catch (IOException e) {
+			log.warn(e.getLocalizedMessage());
+			if (log.isDebugEnabled()) {
+				log.debug("method()", e);
+			}
+		} finally {
+			try { fos.close(); } catch (Exception e) { /* ignore */ }
+		}
+	}
+	
+	private String getExtension(String contentType) {
+		String[] tmp = contentType.split(";");
+		int idx = tmp[0].lastIndexOf('/');
+		char[] ext = (idx != -1) 
+			? tmp[0].substring(idx+1).toCharArray()
+			: tmp[0].toCharArray();
+		StringBuilder buf = new StringBuilder();
+		for (int i=0; i < ext.length; i++) {
+			if (Character.isJavaIdentifierPart(ext[i])) {
+				buf.append(ext[i]);
+			} else {
+				buf.append('_');
+			}
+		}
+		return buf.toString();
+	}
+	
+	Pattern uriPattern = Pattern.compile("http://[-a-zA-Z0-9/\\.]*");
+	
+	private void findURIs(String s, List<URI> list) {
+		Matcher m = uriPattern.matcher(s);
+		while (m.find()) {
+			try {
+				URI uri = new URI(m.group());
+				list.add(uri);
+			} catch (URISyntaxException e) {
+				log.warn(e.getLocalizedMessage());
+				if (log.isDebugEnabled()) {
+					log.debug("method()", e);
+				}
+			}
+		}
+	}
+	
+	private void dumpObject(File dir, String prefix, int count, Object o,
+		String contentType, List<URI> uriList) 
+	{
+		if (o instanceof String) {
+			String s = o.toString();
+			dump(new File(dir, prefix + ".txt"), s.getBytes());
+			findURIs(s, uriList);
+		} else if (o instanceof MimeMultipart) {
+			try {
+				MimeMultipart part = (MimeMultipart) o;
+				int idx = part.getCount();
+				for (int i=0; i < idx; i++) {
+					BodyPart bp = part.getBodyPart(i);
+					dumpObject(dir, prefix + "." + i, count, bp.getContent(), 
+						bp.getContentType(), uriList);
+				}
+			} catch (Exception e) {
+				log.warn("msg " + count + " - " + e.getLocalizedMessage());
+				if (log.isDebugEnabled()) {
+					log.debug("method()", e);
+				}
+			}
+		} else if (o instanceof InputStream) {
+			InputStream in = (InputStream) o;
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(4096);
+			byte[] dst = new byte[4096];
+			int read = 0;
+			try {
+				while ((read = in.read(dst)) != -1) {
+					bos.write(dst, 0, read);
+				}
+			} catch (IOException e) {
+				log.warn("msg " + count + " - " + e.getLocalizedMessage());
+				if (log.isDebugEnabled()) {
+					log.debug("method()", e);
+				}
+			} finally {
+				try { in.close(); } catch (Exception x) { /* ignore */ }
+			}
+			String ext = getExtension(contentType);
+			if (ext.equals("plain") || ext.equals("html") || ext.equals("xml")) {
+				findURIs(new String(bos.toByteArray()), uriList);
+			}
+			dump(new File(dir, prefix + "." + ext), bos.toByteArray());
+		} else if (o instanceof MimeMessage) {
+			MimeMessage m = (MimeMessage) o;
+			try {
+				dumpObject(dir, prefix, count, m.getContent(), 
+					m.getContentType(), uriList);
+			} catch (Exception e) {
+				log.warn("msg " + count + " - " + e.getLocalizedMessage());
+				if (log.isDebugEnabled()) {
+					log.debug("method()", e);
+				}
+			}
+		} else {
+			log.warn("Unable to handle msg " + count + " " 
+				+ o.getClass().getSimpleName() + " " + contentType);
+		}
+	}
+
+	public void dump(File dir, ArrayList<Mail> mails, List<URI> uriList) {
+		if (!dir.exists()) {
+			if (!dir.mkdir()) {
+				log.warn("Unable to create " + dir);
+				return;
+			}
+		}
+		if (!dir.isDirectory()) {
+			log.warn(dir + " is not a directory");
+			return;
+		}
+		if (!dir.canWrite()) {
+			log.warn(dir + " is not writable");
+			return;
+		}
+		int count = 1;
+		for (Mail m : mails) {
+			try {
+				StringWriter w = new StringWriter();
+				PrintWriter p = new PrintWriter(w);
+				p.printf("%04d", count);
+				dumpObject(dir, w.toString(), count, m.getContent(), 
+					m.getContentType(), uriList);
+			} catch (Exception e) {
+				log.warn("msg " + count + " - " + e.getLocalizedMessage());
+				if (log.isDebugEnabled()) {
+					log.debug("method()", e);
+				}
+			}
+			count++;
+		}
+	}
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			System.err.println("Usage: java -cp ... MboxReader mboxFile");
+			System.exit(1);
+		}
+		MboxReader mr = new MboxReader();
+		ArrayList<Mail> mails = mr.read(new File(args[0]));
+		ArrayList<URI> uriList = new ArrayList<URI>();
+		mr.dump(new File("/tmp/mail"), mails, uriList);
+		TreeMap<String, TreeSet<String>> set = new TreeMap<String,TreeSet<String>>();
+		log.info("Found " + uriList.size() + " URLs");
+		StringBuilder buf = new StringBuilder();
+		Whois whois = new Whois();
+		for (URI uri : uriList) {
+//			System.out.println(uri);
+			String host = whois.getTldNormalizedDomain(uri.getHost());
+			buf.append(host);
+			buf.reverse();
+			String dom = buf.toString();
+			TreeSet<String> uset = set.get(dom);
+			if (uset == null) {
+				uset = new TreeSet<String>();
+				set.put(dom, uset);
+			}
+			uset.add(uri.toString());
+			buf.setLength(0);
+		}
+		for (Entry<String, TreeSet<String>> e : set.entrySet()) {
+			buf.append(e.getKey());
+			buf.reverse();
+			buf.append('\n');
+			for (String s : e.getValue()) {
+				buf.append("   ").append(s).append('\n');
+			}
+			System.out.println(buf.toString());
+			buf.setLength(0);
+		}
+		log.info(set.size() + " domains");
+	}
+}
---
+++ src/run.sh
@@ -0,0 +1,109 @@
+#!/bin/ksh
+
+JAVA_HOME=${JAVA_HOME:-/local/apps/jdk}
+
+# additional flags for the java virtual machine
+JVM_FLAGS="-Djava.awt.headless=true"
+
+if [ "$1" != "shutdown" ]; then
+	# enable JMX tools to get information about the server and its stats frome remote
+	JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.port=12345"
+	# if one has no firewalls, dedicated connection machines one would probably
+	# inverse these settings
+	JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.authenticate=false"
+	JVM_FLAGS="$JVM_FLAGS -Dcom.sun.management.jmxremote.ssl=false"
+	
+	# just in case, somebody wants to attach a debugger from remote
+	#JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,address=45678,server=y,suspend=n"
+fi
+
+#=========================================================================
+# no further changes required
+#=========================================================================
+START_CLASS=Version
+
+export LC_CTYPE=en_US
+
+Usage() {
+cat<<EOF
+Usage: ${0} [-h] [shutdown]
+
+    -h        print this help and exit
+EOF
+}
+
+JAVA=`which java 2>/dev/null`
+if [ -n "$JAVA_HOME" ]; then
+	if [ -x ${JAVA_HOME}/bin/java ]; then
+		JAVA=${JAVA_HOME}/bin/java
+	fi
+fi
+
+if [ ! -x "$JAVA" ]; then
+	echo "JVM not found. Set JAVA_HOME or PATH env variable!"
+	exit 1
+fi
+
+if [ -z "$JVM_FLAGS" ]; then
+	JVM_FLAGS=" "
+fi
+
+while getopts "h" option ; do
+	case "$option" in
+		h) Usage; exit 0 ;;
+	esac
+done	
+X=$((OPTIND-1))
+shift $X
+
+# resolv links to base directory
+PRG=$0
+progname=`basename $0`
+while [ -h "$PRG" ] ; do
+	ls=`ls -ld "$PRG"`
+	link=`expr "$ls" : '.*-> \(.*\)$'`
+	if expr "$link" : '.*/.*' > /dev/null; then
+		PRG="$link"
+	else
+		PRG="`dirname $PRG`/$link"
+	fi
+done
+BASE_DIR=`dirname "$PRG"`/..
+
+PRG=$JAVA
+while [ -h "$PRG" ] ; do
+	ls=`ls -ld "$PRG"`
+	link=`expr "$ls" : '.*-> \(.*\)$'`
+	if expr "$link" : '.*/.*' > /dev/null; then
+		PRG="$link"
+	else
+		PRG="`dirname $PRG`/$link"
+	fi
+done
+
+if [ ! -d "${BASE_DIR}/lib" ]; then
+	echo "Base directory $BASE_DIR/lib does not exist. Exiting"
+	exit 2
+fi
+
+# add in the dependency .jar files
+DIRLIBS=${BASE_DIR}/lib/*.jar
+LOCAL_CLASSPATH="${BASE_DIR}/lib"
+for i in $DIRLIBS ; do
+    # if the directory is empty, then it will return the input string
+    # this is stupid, so case for it
+	if [ "$i" != "${DIRLIBS}" ] ; then
+		LOCAL_CLASSPATH="$i":$LOCAL_CLASSPATH
+	fi
+done
+LOCAL_CLASSPATH="-cp ${LOCAL_CLASSPATH}"
+
+if [ -d "${BASE_DIR}/lib/endorsed" ]; then
+	if [ -z "$JVM_FLAGS" ]; then
+		JVM_FLAGS="-Djava.endorsed.dirs=${BASE_DIR}/lib/endorsed"
+	else
+		JVM_FLAGS="$JVM_FLAGS -Djava.endorsed.dirs=${BASE_DIR}/lib/endorsed"
+	fi
+fi
+
+exec $JAVA ${JVM_FLAGS} ${JAVA_OPTS} $LOCAL_CLASSPATH de.ovgu.cs.jelmilter.$START_CLASS $@
